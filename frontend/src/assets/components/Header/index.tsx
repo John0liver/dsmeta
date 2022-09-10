e7a0e17f372692dc80b5d2b770d4c6728677d56c
John0liver <jhonatas404@outlook.com>
2022-09-09 22:37:09 -0300

    useState

--- frontend/src/assets/components/Header/index.tsx
+++ frontend/src/assets/components/Header/index.tsx
@@ -1,5 +1,5 @@
-import logo from '../../img/logo.svg'
-import './styles.css'
+import logo from '../../img/logo.svg';
+import './styles.css';
 
 function Header() {
     return (
@@ -12,4 +12,4 @@ function Header() {
     )
 }
 
-export default Header
+export default Header;
