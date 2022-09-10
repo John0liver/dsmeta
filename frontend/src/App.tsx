a9076491397646f2b5dc9c1e951adea159d8b0e5
John0liver <jhonatas404@outlook.com>
2022-09-09 21:27:52 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,12 @@
+import NotificationButton from "./assets/components/NotificationButton"
 
 
 function App() {
-  return(
-    <h1>ola mundo</h1>
+  return (
+    <>
+      <h1>ola mundo</h1>
+      <NotificationButton />
+    </>
   )
 }
 
